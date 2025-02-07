610b39a55c3741d9c553d43518e3b31e94ec3035
Benjamin BENOIT <benjaminbenoit@MacBook-Air-de-Benjamin.local>
2025-02-07 13:59:59 +0100

    fix : webserver en server et config extensions js

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,14 @@ const nextConfig = {
     output: 'standalone',
     experimental: {
     },
-    webServer: {
+    server: {
         port: parseInt(process.env.PORT, 10) || 8080,
         hostname: '0.0.0.0'
+    },
+    webpack: (config) => {
+        config.resolve.extensions = ['.js', '.jsx', '.ts', '.tsx', ...config.resolve.extensions];
+        return config;
     }
 }
+
 export default nextConfig;
